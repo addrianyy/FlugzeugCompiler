8b3951a8f579af8dfc53ed8f745bc05829f6b27d
addrianyy <adrianvpl@gmail.com>
2022-03-06 15:44:23 +0100

    Remove testing code from main

--- src/main.cpp
+++ src/main.cpp
@@ -35,86 +35,6 @@
 
 using namespace flugzeug;
 
-static void test_validation(Context& context) {
-  Type* i1 = context.get_i1_ty();
-  Type* i32 = context.get_i32_ty();
-  Type* i64 = context.get_i64_ty();
-
-  const auto m = context.create_module();
-  const auto f = m->create_function(i32, "test", {});
-
-  const auto entry = f->create_block();
-  const auto a = f->create_block();
-  const auto b = f->create_block();
-  const auto merge = f->create_block();
-
-  InstructionInserter inserter;
-  inserter.set_insertion_block(entry);
-  inserter.cond_branch(i1->get_zero(), a, b);
-
-  inserter.set_insertion_block(a);
-  auto x = inserter.add(i32->get_constant(2), i32->get_constant(3));
-  inserter.branch(merge);
-
-  inserter.set_insertion_block(b);
-  auto y = inserter.add(i32->get_constant(4), i32->get_constant(5));
-  inserter.branch(merge);
-
-  inserter.set_insertion_block(merge);
-  auto z = inserter.neg(y);
-  auto phi1 = inserter.phi({{entry, x}, {b, y}});
-  inserter.ret(i32->get_constant(1));
-  inserter.ret(i64->get_constant(3));
-
-  m->print();
-  m->validate(ValidationBehaviour::ErrorsAreFatal);
-  m->destroy();
-}
-
-static void test_minimization(Context& context) {
-  Type* i32 = context.get_i32_ty();
-
-  const auto m = context.create_module();
-  const auto f = m->create_function(i32, "test", {i32, i32, i32, i32, i32});
-
-  const auto a = f->get_parameter(0);
-  const auto b = f->get_parameter(1);
-  const auto c = f->get_parameter(2);
-  const auto d = f->get_parameter(3);
-  const auto e = f->get_parameter(4);
-
-  const auto entry = f->create_block();
-  const auto b1 = f->create_block();
-  const auto b2 = f->create_block();
-  const auto b3 = f->create_block();
-
-  InstructionInserter inserter(entry);
-  const auto cond = inserter.compare_sgte(a, i32->get_one());
-  inserter.cond_branch(cond, b1, b2);
-
-  inserter.set_insertion_block(b1);
-  const auto phi1 = inserter.phi(i32);
-  inserter.branch(b2);
-
-  inserter.set_insertion_block(b2);
-  const auto phi2 = inserter.phi(i32);
-  inserter.cond_branch(cond, b1, b3);
-
-  inserter.set_insertion_block(b3);
-  inserter.ret(i32->get_one());
-
-  phi1->add_incoming(entry, a);
-  phi1->add_incoming(b2, phi2);
-
-  phi2->add_incoming(entry, a);
-  phi2->add_incoming(b1, phi1);
-
-  opt::PhiMinimization::run(f);
-
-  m->print();
-  m->destroy();
-}
-
 static void optimize_function(Function* f) {
   while (true) {
     bool did_something = false;
@@ -142,16 +62,6 @@ static void optimize_function(Function* f) {
       break;
     }
   }
-}
-
-static void debug_print_loops(Function* f) {
-  const auto loops = analysis::analyze_function_loops(f);
-
-  log_debug("{}:", f->get_name());
-  for (const auto& loop : loops) {
-    loop->debug_print("  ");
-  }
-  log_debug("");
 }
 
 int main() {
